c41bb38908b2e607c5573ba21064142646c61ac5
yaobw <bowen.yao@ideacome.com>
2020-05-21 09:02:43 +0800

    delete some code

--- src/main/java/algorithm/easy/IntegerReverse.java
+++ src/main/java/algorithm/easy/IntegerReverse.java
@@ -37,7 +37,6 @@ public class IntegerReverse {
     public static int reverse(int x) {
         int result = 0;
         while (x != 0) {
-//            if (result > 214748364 || result < -214748364) {
             if (result > Integer.MAX_VALUE/10 || result < Integer.MIN_VALUE/10 ) {
                 return 0;
             }
